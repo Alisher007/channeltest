565387a626d19f4befefffcc8f84915b5a309491
Alisher007 <alisherx07@gmail.com>
2022-04-11 17:40:10 +0400

    group chat updated

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# async_to_sync(self.channel_layer.group_send)(
+#             self.room_group_name,
+#             {
+#                 'type':'chat_message',
+#                 'message': message
+#             }
+#         )
+    
+
+    # def chat_message(self, event):
+    #     message = event['message']
+
+    #     self.send(text_data=json.dumps({
+    #         'type':'chat',
+    #         'message': message
+    #         })
+    #     )
